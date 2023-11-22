cd62f44a91b5dae96ad369a00eef62c374bcb56a
Aliksalot <alexkolev05@gmail.com>
2023-11-22 07:47:17 +0200

    Changes to whole infrastructure

--- src/routes/page.js
+++ src/routes/page.js
@@ -5,12 +5,12 @@ const path = require('path')
 
 const pagesPath = path.join(__dirname, '../client/pages')
 
-router.get('/page1', (req, res) => {
-    res.sendFile(path.join(pagesPath, 'index.html'))
+router.get('/survey', (req, res) => {
+    res.sendFile(path.join(pagesPath, 'survey.html'))
 })
 
-router.get('/page2', (req, res) => {
-    res.sendFile(path.join(pagesPath, 'index1.html'))
+router.get('/admin-panel', (req, res) => {
+    res.sendFile(path.join(pagesPath, 'admin-panel.html'))
 })
 
 router.get('/config', (req, res) => {
